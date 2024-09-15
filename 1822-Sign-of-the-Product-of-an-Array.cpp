c8b8fb65c8c3c0f822cff0166ea3cc81f906a3c4
Jaiman Singh Guliani <123560972+JaimanSingh@users.noreply.github.com>
2024-09-15 22:36:04 +0530

    [1822] [Time Beats: 81.59%] [Memory Beats: 68.97%] - LeetPush

---
+++ 1822-Sign-of-the-Product-of-an-Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg_cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) return 0;
+            if(nums[i]<0) neg_cnt++;
+        }
+        if(neg_cnt%2 == 1) return -1;
+        return 1;
+    }
+};
