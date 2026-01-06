f3480e706810ae3142d6ede1290daab377de05f7
Jaiman Singh Guliani <123560972+JaimanSingh@users.noreply.github.com>
2026-01-06 11:34:04 +0530

    [7] [Time Beats: 5.31%] [Memory Beats: 81.77%] - LeetPush

--- Reverse-Integer.cpp
+++ Reverse-Integer.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    int reverse(int x) {
-        
-        int ans = 0;
-        while (x!=0) {
-
-            int digit = x%10;
-
-            if ((ans > INT_MAX/10) || (ans < INT_MIN/10)) {
-                return 0; 
-            }
-
-            ans = (ans*10) + digit;
-            x = x/10;
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    int reverse(int x) {
+4        
+5        int ans = 0;
+6        while (x!=0) {
+7
+8            int digit = x%10;
+9
+10            if ((ans > INT_MAX/10) || (ans < INT_MIN/10)) {
+11                return 0; 
+12            }
+13
+14            ans = (ans*10) + digit;
+15            x = x/10;
+16        }
+17        return ans;
+18    }
+19};
